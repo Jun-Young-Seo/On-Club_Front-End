97ef753d27771ae474cbc1ba57c7a1908badbf93
Jun Young <aa2030822@naver.com>
2025-03-12 15:40:01 +0900

    Recreate main branch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Route,Router,Routes, useLocation } from 'react-router';
 import Main from './Main/Main';
-
+//asdasd
 function App() {
   const location = useLocation();
   return (
